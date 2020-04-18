fcf89ce0ced24d74f53ad1beb1dc8322bdc8956d
yngtodd <young.todd.mk@gmail.com>
2020-04-18 17:22:56 -0400

    Fix docstrings
    The markdown was formatting this a little weird.

--- src/data.jl
+++ src/data.jl
@@ -8,20 +8,27 @@ Load a numpy array and transpose it so that it is
 in column-major form. This lets us make use of the 
 same data files for our models in Python.
 
-Say you created a numpy array created by:
+Say you created a numpy array created by, and saved
+it as `my_array.npy`:
 
+```
 np.arange(10).reshape((2, 5))
+
 >>> array([[0, 1, 2, 3, 4],
            [5, 6, 7, 8, 9]])
+```
 
-loading that array with `load_numpy`, we would get
+loading that array with `load_numpy("my_array.npy")`, 
+we would get
 
+```
 5×2 LinearAlgebra.Transpose{Int64,Array{Int64,2}}:
  0  5
  1  6
  2  7
  3  8
  4  9
+```
 """
 function load_numpy(path)
     arry = NPZ.npzread(path)
@@ -34,7 +41,7 @@ end
 
 Split a dataset of x, y pairs into two sets with a given ratio.
 This will return a pair of tuples with the `ratio` percent of
-of the data belonging to the first tuple.
+of the data belonging to the first tuple:
 
 ```
 (x_split1, y_split1), (x_split2, y_split2)
