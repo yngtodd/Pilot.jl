9ec8c0eb39b8c690013c2c6961a49c11a742059c
yngtodd <young.todd.mk@gmail.com>
2020-04-19 02:09:11 -0400

    Add docstrings
    This will be the first doctsring test of the library, and will show
    users how to make use of the embedding layer.

--- src/model/embedding.jl
+++ src/model/embedding.jl
@@ -6,16 +6,44 @@ struct Embedding
 end
 
 
-Embedding(num_vocab, embed_dim) = Embedding(Flux.glorot_normal(num_vocab, embed_dim))
+@Flux.treelike Embedding
 
 
 (e::Embedding)(x) = e.table[x, :]
 
 
+"""
+    Embedding(num_vocab, embed_dim)
+
+Construct an embedding layer for Flux models. This 
+is a mapping from the dimension of the model's 
+`num_vocab`, the number of unique vocabulary
+terms in a corpus, to some embedding dimension, `embed_dim`.
+
+# Examples
+```julia-repl
+julia> m = Embeddig(10, 5)
+
+```
+"""
+Embedding(num_vocab, embed_dim) = Embedding(Flux.glorot_normal(num_vocab, embed_dim))
+
+
+"""
+Forward pass of the Embedding layer. 
+
+Given an array of size (input_dim, batch_size), this will 
+embed the information to (input_dim, embed_dim, batch_size).
+
+```@doctest
+input = rand(1:100, (10, 32))
+m = Pilot.Embedding(100, 16)
+out = m(input)
+size(out)
+(10, 16, 32)
+```
+"""
 function (e::Embedding)(x::AbstractArray{T, 2}) where {T}
     out = e.table[x, :]
-    return(permutedims(out, (1, 3, 2)))
+    permutedims(out, (1, 3, 2))
 end
-
-
-@Flux.treelike Embedding
