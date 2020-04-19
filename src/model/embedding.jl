78ccd62b6908cd4c31985760bc6f5a50df10bddd
yngtodd <young.todd.mk@gmail.com>
2020-04-19 15:25:59 -0400

    Remove whitespace
    This is just to test to see if the labeler workflow is running
    properly.

--- src/model/embedding.jl
+++ src/model/embedding.jl
@@ -15,8 +15,8 @@ end
 """
     Embedding(num_vocab, embed_dim)
 
-Construct an embedding layer for Flux models. This 
-is a mapping from the dimension of the model's 
+Construct an embedding layer for Flux models. This
+is a mapping from the dimension of the model's
 `num_vocab`, the number of unique vocabulary
 terms in a corpus, to some embedding dimension, `embed_dim`.
 
@@ -32,7 +32,7 @@ Embedding(num_vocab, embed_dim) = Embedding(Flux.glorot_normal(num_vocab, embed_
 """
 Forward pass of the Embedding layer. 
 
-Given an array of size (input_dim, batch_size), this will 
+Given an array of size (input_dim, batch_size), this will
 embed the information to (input_dim, embed_dim, batch_size).
 
 # Examples
